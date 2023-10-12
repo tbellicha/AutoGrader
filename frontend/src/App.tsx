fcf9dfed00e4b7eb9e927746c579e173ae768808
Ali Ahmed <78716016+a-amd@users.noreply.github.com>
2023-10-11 23:50:35 -0700

    Added React project template under frontend directory

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+     <div className="upload-container">
+      This is where the multi-part form for uploading zips and files will go.
+      This is currently a placeholder for our project skeleton.
+      </div> 
+      <p>
+        This is a placeholder paragraph...
+      </p>
+    </>
+  )
+}
+
+export default App
