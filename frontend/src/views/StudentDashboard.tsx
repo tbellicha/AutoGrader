ca283ed8b1c32467f26237575552d8c4aa1afcfb
Ali Ahmed <78716016+a-amd@users.noreply.github.com>
2023-11-14 16:45:01 -0800

    Add Student Dashboard view.

---
+++ frontend/src/views/StudentDashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StudentDashboard: React.FC<any> = () => {
+
+    return (
+        <>
+            <h1>Student Dashboard</h1>
+        </>
+    );
+};
+
+export default StudentDashboard;
